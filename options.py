7246acffcfc8b51c1c12a3b96e8d7303e9f78645
Ankush Chatterjee <ac96guitar@gmail.com>
2016-12-20 17:40:43 +0530

    Prefix added

--- options.py
+++ options.py
@@ -2,14 +2,39 @@ import tensorflow as tf
 
 
 class Options(object):
-	batch_size = 32
-	train_epochs = 100
-	lstm_layers = 1
-	keep_prob = 0.8
-	lstm_dim = 128
 
-	num_classes = 4816
+	# training
+	batch_size = 64
+	sample_size = 64
+	train_epochs = 100
 	lrate = 1e-4
 	beta1 = 0.5
 
+
+	# architecture
+	z_dim = 100
+	gf_dim = 64
+	df_dim = 64
+	gfc_dim = 1024
+	dfc_dim = 1024
+	c_dim = 3
+	mask_penalty = 0.1
+
+
+	# data
+	video_shape = [32,64,64,3]
+	train_dataset = './data/csail/videos-0'
+
+
+	def prefix():
+		prefix = "videogan_"
+		prefix += "bs_%d_" % (Options.batch_size)
+		prefix += "ss_%d_" % (Options.sample_size)
+		prefix += "te_%d_" % (Options.train_epochs)
+		prefix += "lr_%d_" % (Options.lrate)
+
+		
+	# Model saving
+	checkpoint_dir = "./checkpoints/" + Options.prefix()
+	
     
